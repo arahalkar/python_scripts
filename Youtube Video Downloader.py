163540fd427e6bb149c22acfdd7e1f3f6e5e1525
Amit Rahalkar <arahalkar@gmail.com>
2021-02-22 15:36:58 -0500

    adding a few explainatory comments to the code

--- Youtube Video Downloader.py
+++ Youtube Video Downloader.py
@@ -12,9 +12,12 @@ while True:
         break 
 
 for i in urls_list:
+    #check whether it is a comment, or any other text without a URL in it 
     url = i.rsplit('https://')
+    #proceed only if the line contains a URL that starts with HTTPS
     if(len(url)>1): 
         try:
+            #prepend the https:// back to the split URL to make it fully valid
             url = ('https://').join(url)
             print(url)
             video=YouTube(url)
