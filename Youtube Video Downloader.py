8d2b7dc2fbea7aa37a58a5727f5ae2926b0d17b4
Amit Rahalkar <arahalkar@gmail.com>
2021-02-22 10:49:34 -0500

    adding 2nd comment from local machine

--- Youtube Video Downloader.py
+++ Youtube Video Downloader.py
@@ -15,7 +15,8 @@ for i in urls_list:
     #url = i[3:]
     print(i.rfind('https'))
     print(i.rsplit('//'))
-    #added in the webbrowser
+    #added in the webbrowser 
+    #Now add in the VS code     
     
     #if url[:5] != 'https':
      #   continue
