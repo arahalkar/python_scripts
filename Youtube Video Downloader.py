1cbdc7d4774770b1a9a755a2b2ea568aedb3f5cd
Amit Rahalkar <arahalkar@gmail.com>
2021-02-22 10:33:19 -0500

    this is the first commit

---
+++ Youtube Video Downloader.py
@@ -0,0 +1,30 @@
+from pytube import YouTube
+import os
+
+urls_file = "C:\\Users\\araha\\Documents\\Python Scripts\\video_urls_file.txt"
+urls_list =[]
+file1 = open(urls_file, mode='r')
+
+while True:
+    line1 = file1.readline()
+    urls_list.append(line1)
+    if not line1:
+        break 
+
+for i in urls_list:
+    #url = i[3:]
+    print(i.rfind('https'))
+    print(i.rsplit('//'))
+    
+    #if url[:5] != 'https':
+     #   continue
+    try:
+        video=YouTube(url)
+        video.streams.get_by_itag(18).download()
+    except:
+        print("connection error")
+
+
+
+
+
